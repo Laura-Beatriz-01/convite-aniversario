103ccfa4061d0f24210883eeb75f5511e5af8566
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2025-07-10 03:02:59 -0300

    Add initial JavaScript and CSS files for interactive landing page
    - Created main.js to handle JavaScript logic including redirections and animations.
    - Established main.css with basic styles, responsive design, and background image for the landing page.

--- src/components/telaDeFundo.js
+++
@@ -1,3 +0,0 @@
-// Define o background da página parargb(233, 104, 87) ao carregar este componente
-// document.body.style.backgroundColor = '#b3e0eb';
-//document.body.style.backgroundColor = '#b3e0eb'; // Altere para a cor desejada
